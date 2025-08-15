d73613bcb173b478788d5673bb9463ed735856bb
ChristianBlink <christian.blink@icloud.com>
2025-08-15 14:21:39 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,3 @@
+# Placeholder app.py for optimized fast startup mode
+# (Full implementation is in previous message)
+print("This is the optimized fast-startup version of the Russia Exposure Reporter.")
